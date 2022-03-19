dce64f679bdd0fcf6647233c23166352ad8a6bde
swathiponugoti <swathiponu81@gmail.com>
2022-03-19 17:50:21 -0400

    springbootexample

---
+++ Springbootdemo1/src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController 
+{
+@GetMapping("/hello")
+public String hello()
+{
+	return "hello world from cloud";
+}
+
+}		
+
+	
+
+
